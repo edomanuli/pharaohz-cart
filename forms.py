72febca28ba421421d0d3adbfb82814370f012a5
edomanuli <edomanuli@gmail.com>
2023-09-08 20:11:52 -0500

    Created the login and registration pages

---
+++ forms.py
@@ -0,0 +1,21 @@
+"""Flask form"""
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    """Registration Form"""
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    username = StringField('username', validators=[DataRequired(), Length(min=2, max=16)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+    
+class LoginForm(FlaskForm):
+    """Login Form"""
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
